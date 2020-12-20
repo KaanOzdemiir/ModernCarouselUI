bc78e3c524e74a0fd2e5bde745a670c4d96d1de2
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2020-12-20 17:13:38 +0300

    Initial Commit

---
+++ ModernCarouselUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModernCarouselUI
+//
+//  Created by Kaan Ozdemir on 20.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModernCarouselUI/ModernCarouselUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernCarouselUIApp.swift
+//  ModernCarouselUI
+//
+//  Created by Kaan Ozdemir on 20.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ModernCarouselUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
